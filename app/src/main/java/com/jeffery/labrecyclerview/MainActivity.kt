30cda0ace687031e12e868f1c31386dd738a2730
zhaojifeng <zhaojifengeral@163.com>
2017-11-21 10:39:36 +0800

    add something about head、foot、loading view

--- app/src/main/java/com/jeffery/labrecyclerview/MainActivity.kt
+++ app/src/main/java/com/jeffery/labrecyclerview/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.jeffery.labrecyclerview
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val data: ArrayList<String> = ArrayList()
+        val context: Context = this
+        val adapter: LabRecyclerViewAdapter<String>
+                = object : LabRecyclerViewAdapter<String>(context, data, 1, 2) {
+            override fun setConvertView(holder: LabRecyclerViewViewHolder, item: String, position: Int) {
+
+            }
+        }
     }
 }
--- lab/src/main/java/com/jeffery/labrecyclerview/LabRecyclerViewAdapter.kt
+++ lab/src/main/java/com/jeffery/labrecyclerview/LabRecyclerViewAdapter.kt
@@ -1,9 +1,12 @@
 package com.jeffery.labrecyclerview
 
 import android.content.Context
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.View
 import android.view.ViewGroup
+import java.util.*
 
 /**
  *
@@ -12,10 +15,13 @@ import android.view.ViewGroup
  */
 abstract class LabRecyclerViewAdapter<T>(
         protected val mContext: Context,
-        protected val mData: List<T>,
+        protected val mData: ArrayList<T>,
         vararg layoutIds: Int
 ) : RecyclerView.Adapter<LabRecyclerViewViewHolder>() {
-    private val mLayoutIds:IntArray = layoutIds
+
+    private val mLayoutIds: IntArray = layoutIds
+    private var mRecyclerView: RecyclerView? = null
+    private var mRecyclerManager: RecyclerView.LayoutManager? = null
 
     companion object {
         private val VIEW_TYPE_HEADER: Int = 0x10000001
@@ -23,25 +29,144 @@ abstract class LabRecyclerViewAdapter<T>(
         private val VIEW_TYPE_LOADING: Int = 0x10000003
     }
 
+    fun getData(): List<T> = mData
+
     private var mHeaderView: View? = null
-        set(value) {
-            notifyDataSetChanged()
-        }
     private var mFooterView: View? = null
     private var mLoadingView: View? = null
     private var mEmptyView: View? = null
 
 
+    fun setHeaderView(view: View) {
+        mHeaderView = view
+        notifyDataSetChanged()
+    }
+
+    fun setFooterView(view: View) {
+        mFooterView = view
+        notifyDataSetChanged()
+    }
+
+    fun setLoadingView(view: View) {
+        mLoadingView = view
+        notifyDataSetChanged()
+        mRecyclerView?.smoothScrollToPosition(itemCount - 1)
+    }
+
+    fun setEmptyView(view: View) {
+        mEmptyView = view
+    }
+
+    fun removeHeaderView() {
+        if (hasHeaderView()) {
+            mHeaderView = null
+            notifyDataSetChanged()
+        }
+    }
+
+    fun removeFooterView() {
+        if (hasFooterView()) {
+            mFooterView = null
+            notifyDataSetChanged()
+        }
+    }
+
+    fun removeLoadingView() {
+        if (hasLoadingView()) {
+            mLoadingView = null
+            notifyDataSetChanged()
+        }
+    }
+
+    fun removeEmptyView() {
+        if (hasEmptyView()) {
+            mEmptyView = null
+        }
+    }
+
+
+    private fun hasHeaderView(): Boolean = mHeaderView != null
+
+    private fun hasFooterView(): Boolean = mFooterView != null
+
+    private fun hasLoadingView(): Boolean = mLoadingView != null
+
+    private fun hasEmptyView(): Boolean = mEmptyView != null
+
+
+    private fun needShowHeaderView(position: Int): Boolean
+            = position == 0 && hasHeaderView()
+
+    private fun needShowFooterView(position: Int): Boolean
+            = (position == itemCount - 1 && hasFooterView() && !hasLoadingView())
+            || (position == itemCount - 2 && hasFooterView() && hasLoadingView())
+
+    private fun needShowLoadingView(position: Int): Boolean
+            = position == itemCount - 1 && hasLoadingView()
+
+
     override fun getItemCount(): Int {
         setEmptyViewState()
+        return mData.size + getLoadingViewCount() + getHeaderViewCount() + getFooterViewCount()
+    }
+
+    fun getRealItemCount(): Int {
         return mData.size
     }
 
-    private fun setEmptyViewState() {
+    private fun getLoadingViewCount(): Int = if (hasLoadingView()) 1 else 0
+    private fun getHeaderViewCount(): Int = if (hasHeaderView()) 1 else 0
+    private fun getFooterViewCount(): Int = if (hasFooterView()) 1 else 0
 
+    private fun setEmptyViewState() {
+        if (hasEmptyView()) {
+            if (getRealItemCount() == 0) {
+                mEmptyView?.visibility = View.VISIBLE
+                mHeaderView?.visibility = View.GONE
+                mFooterView?.visibility = View.GONE
+            } else {
+                mEmptyView?.visibility = View.GONE
+                mHeaderView?.visibility = View.VISIBLE
+                mFooterView?.visibility = View.VISIBLE
+            }
+        }
     }
 
-    fun getData(): List<T> = mData
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            needShowHeaderView(position) -> VIEW_TYPE_HEADER
+            needShowFooterView(position) -> VIEW_TYPE_FOOTER
+            needShowLoadingView(position) -> VIEW_TYPE_LOADING
+            else -> getItemType(position)
+        }
+    }
+
+    protected fun getItemType(position: Int): Int = 0
+
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView?) {
+        super.onAttachedToRecyclerView(recyclerView)
+        if (mRecyclerManager == null) {
+            mRecyclerView = recyclerView
+            mRecyclerManager = recyclerView?.layoutManager
+            setGridWight(mRecyclerManager)
+        }
+    }
+
+    private fun setGridWight(manager: RecyclerView.LayoutManager?) {
+        if (manager != null) {
+            if (manager is GridLayoutManager) {
+                manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                    override fun getSpanSize(position: Int): Int {
+                        return if (needShowHeaderView(position) || needShowFooterView(position) || needShowLoadingView(position)) {
+                            manager.spanCount
+                        } else 1
+                    }
+                }
+            }
+        }
+    }
+
 
     override fun onBindViewHolder(holder: LabRecyclerViewViewHolder?, position: Int) {
         if (holder != null) {
@@ -71,7 +196,17 @@ abstract class LabRecyclerViewAdapter<T>(
 
 
         if (itemView != null) {// not normal view
-
+            if (mRecyclerManager is StaggeredGridLayoutManager) {
+                val targetParams = itemView.layoutParams
+                val staggerLayoutParams: StaggeredGridLayoutManager.LayoutParams
+                staggerLayoutParams = if (targetParams != null) {
+                    StaggeredGridLayoutManager.LayoutParams(targetParams.width, targetParams.height)
+                } else {
+                    StaggeredGridLayoutManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                }
+                staggerLayoutParams.isFullSpan = true
+                itemView.layoutParams = staggerLayoutParams
+            }
             holder = LabRecyclerViewViewHolder.createViewHolder(mContext, itemView, viewType)
         }
 
@@ -81,14 +216,68 @@ abstract class LabRecyclerViewAdapter<T>(
     abstract fun setConvertView(holder: LabRecyclerViewViewHolder, item: T, position: Int)
 
 
-    private fun isHasHeaderView(): Boolean = mHeaderView != null
+    /**
+     * 添加单条数据
+     *
+     * @param position
+     * @param t
+     */
+    fun addItemData(position: Int, t: T) {
+        mData.add(position, t)
+        notifyItemInserted(position)
+    }
 
-    private fun isHasFooterView(): Boolean = mFooterView != null
+    /**
+     * 添加单条数据
+     *
+     * @param t
+     */
+    fun addItemData(t: T) {
+        val size = mData.size
+        mData.add(size, t)
+        notifyItemInserted(size)
+    }
 
-    private fun isHasLoadingView(): Boolean = mLoadingView != null
+    /**
+     * 移除单条数据
+     *
+     * @param position
+     */
+    fun removeItemData(position: Int) {
+        mData.removeAt(position)
+        notifyItemRemoved(position)
+    }
 
-    private fun isHasEmptyView(): Boolean = mEmptyView != null
+    /**
+     * 修改单条数据
+     *
+     * @param position
+     * @param t
+     */
+    fun changItemData(position: Int, t: T) {
+        mData.set(position, t)
+        notifyItemChanged(position)
+    }
 
+    /**
+     * 刷新数据
+     *
+     * @param newData
+     */
+    fun flushData(newData: List<T>) {
+        mData.clear()
+        this.addNewData(newData)
+    }
+
+    /**
+     * 添加新数据集合
+     *
+     * @param newData
+     */
+    fun addNewData(newData: List<T>) {
+        mData.addAll(newData)
+        notifyDataSetChanged()
+    }
 
 }
 
